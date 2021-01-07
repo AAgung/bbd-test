8a7cc57cb1355c2c74803d8b0fcedcc33b04a184
Agung Budi Santoso <santosobudiagung9@gmail.com>
2021-01-07 11:27:28 +0700

    [Layout] Sidebar

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,39 @@
 <template>
   <div id="app">
-    <div id="nav" class="mt-4 mb-2">
-      <router-link to="/" exact>Home</router-link> |
-      <router-link to="/list-client">Client</router-link>
-    </div>
-
-    <div class="container">
-      <div class="row">
-        <div class="col-12">
-          <router-view />
+    <div class="d-flex" id="wrapper">
+      <!-- Sidebar -->
+      <div class="bg-light border-right" id="sidebar-wrapper">
+        <div class="list-group list-group-flush">
+          <router-link class="list-group-item list-group-item-action bg-light" to="/" exact>
+            <b-icon icon="house"></b-icon>
+          </router-link>
+          <router-link class="list-group-item list-group-item-action bg-light" to="/list-client">
+            <b-icon icon="people"></b-icon>
+          </router-link>
         </div>
       </div>
-    </div>
+      <!-- /#sidebar-wrapper -->
 
+      <!-- Page Content -->
+      <div class="container" id="page-content-wrapper">
+        <div class="row">
+          <div class="col-12">
+            <router-view />
+          </div>
+        </div>
+      </div>
+      <!-- /#Page Content -->
+    </div>
   </div>
 </template>
 
 <style lang="scss">
+  @import './assets/css/custom.css';
+
   #app {
     font-family: Avenir, Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     color: #2c3e50;
-  }
-
-  #nav {
-    text-align: center;
-    a {
-      font-weight: bold;
-      color: #2c3e50;
-
-      &.router-link-exact-active {
-        color: #42b983;
-      }
-    }
   }
 </style>
